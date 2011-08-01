3c7d8a996aa684b724e7de73fb99becbdc34192d
Axel Nitzschner <info@anito.de>
2011-08-02 00:16:47 +0200

    Move Up

--- app/webroot/js/anito_.js
+++ app/webroot/js/anito_.js
@@ -1745,7 +1745,7 @@ var Login = Class.create({
                     $(this.trigger).toggleClassName('down');
                     var input = $(this.trigger);
                     if(input.value == 'Login') {
-                        value = 'Move Up Login';
+                        value = 'Move Up';
                         title = 'Hide Login Panel';
                     } else {
                         value = 'Login';
